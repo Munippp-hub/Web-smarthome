2a4b0e8fab2bd8a2284121e5c69a4defc0a57f8a
Hanif <Muhammadhanifabdullah2515@gmail.com>
2026-04-23 15:20:43 +0700

    init

--- script.js
+++ script.js
@@ -15,3 +15,24 @@ const hideMenu = () => {
 
 closeMenu.addEventListener('click', hideMenu);
 overlay.addEventListener('click', hideMenu);
+
+function toggleFaq(element) {
+    const allItems = document.querySelectorAll('.faq-items');
+    const content = element.querySelector('.faq-content');
+    const img = element.querySelector('img');
+    const openIcon = "/assets/open.svg";
+    const closeIcon = "/assets/close.svg";
+    const isOpening = ! content.style.maxHeight;
+
+    allItems.forEach(item => {
+        const itemContent = item.querySelector('.faq-content');
+        const itemImg = item.querySelector('img');
+        itemContent.style.maxHeight = null;
+        itemImg.src = openIcon
+    });
+
+    if (isOpening) {
+        content.style.maxHeight = content.scrollHeight + "px";
+        img.src = closeIcon;
+    }
+}
